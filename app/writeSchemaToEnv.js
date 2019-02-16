17125361cea80f327cc2641d91e807e4597b09c0
User Jbelich <jbelich@pilsudski.local>
2019-02-16 03:58:06 -0800

    add cloudfront

---
+++ app/writeSchemaToEnv.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const schema = JSON.parse(fs.readFileSync('./awsconfiguration.json'))
+
+// console.log(schema);
+
+process.stdout.write('AWS_AUTH_REGION=' + schema.CognitoUserPool.Default.Region + '\n')
+process.stdout.write('AWS_AUTH_POOL=' + schema.CognitoUserPool.Default.PoolId + '\n')
+process.stdout.write('AWS_AUTH_CLIENT=' + schema.CognitoUserPool.Default.AppClientId + '\n')
+process.stdout.write('AWS_API_ENDPOINT=' + schema.APIGateway.ApiGatewayRestApi.Endpoint + '\n')
+process.stdout.write('AWS_API_REGION=' + schema.APIGateway.ApiGatewayRestApi.Region + '\n')
